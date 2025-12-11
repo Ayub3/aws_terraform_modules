f7909d7e42d187561bdab69494d2bee0008e3100
Ayub Mohamud <ayubmohamud@Ayubs-MacBook-Pro.local>
2025-12-11 16:55:48 +0000

    fix: index igw in route with 0

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -73,7 +73,7 @@ resource "aws_route_table" "private_rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.ngw[count.index].id
+    gateway_id = aws_nat_gateway.ngw[0].id
   }
 
   tags = var.aws_vpc.tags
